fb876873e87208a68b5c4b5ee0564eabce492f1c
Petar Susac <petar.susac@fer.hr>
2024-05-04 16:31:46 +0200

    Implement UART RX

--- app/src/uart/inc/uart.h
+++ app/src/uart/inc/uart.h
@@ -5,10 +5,16 @@
 #include <stddef.h>
 #include <string.h>
 
+#define UART_RX_BUF_LEN (20U)
+
 #define UART_END_BYTE (0x00)
 
 #define UART_MSG_CONN_OK "CONNOK"
+#define UART_MSG_START "START"
 
+typedef void (*uart_rx_cb_t)(const uint8_t *p_data, size_t len);
+
+int uart_register_rx_cb(uart_rx_cb_t p_cb);
 int uart_send_blocking(const uint8_t *p_data, size_t len);
 
 #endif /*_UART_H_ */
--- app/src/uart/src/uart.c
+++ app/src/uart/src/uart.c
@@ -2,8 +2,33 @@
 
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/uart.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(uart, LOG_LEVEL_DBG);
 
 static const struct device *const p_uart_dev = DEVICE_DT_GET(DT_NODELABEL(uart1));
+
+static void char_rx_cb(const struct device *dev, void *user_data);
+
+static uart_rx_cb_t p_user_cb;
+static uint8_t rx_buf[UART_RX_BUF_LEN];
+static size_t rx_buf_idx;
+
+int uart_register_rx_cb(uart_rx_cb_t p_cb)
+{
+    int err;
+
+    p_user_cb = p_cb;
+    rx_buf_idx = 0;
+
+    err = uart_irq_callback_user_data_set(p_uart_dev, char_rx_cb, NULL);
+    if (err < 0)
+    {
+        LOG_ERR("UART IRQ cb register error %d", err);
+    }
+
+    return err;
+}
 
 int uart_send_blocking(const uint8_t *p_data, size_t len)
 {
@@ -15,4 +40,41 @@ int uart_send_blocking(const uint8_t *p_data, size_t len)
     uart_poll_out(p_uart_dev, UART_END_BYTE);
 
     return 0;
+}
+
+static void char_rx_cb(const struct device *dev, void *user_data)
+{
+    uint8_t c;
+
+    if (!uart_irq_update(dev)) {
+		return;
+	}
+
+    if (!uart_irq_rx_ready(dev)) {
+		return;
+	}
+
+    // Read UART FIFO byte by byte
+    while (1 == uart_fifo_read(dev, &c, 1))
+    {
+        // If the end of message is received, call the user cb and clear the
+        // buffer
+        if (UART_END_BYTE == c)
+        {
+            if (p_user_cb)
+            {
+                p_user_cb(rx_buf, rx_buf_idx);
+            }
+
+            rx_buf_idx = 0;
+        }
+        else if (rx_buf_idx < UART_RX_BUF_LEN)
+        {
+            rx_buf[rx_buf_idx++] = c;
+        }
+        else
+        {
+            LOG_WRN("UART char dropped");
+        }
+	}
 }
